9b230deae456152f5e3b88ef9903f53b1520add7
andrewflower-rs <arnaldomahone@hotmail.com>
2024-03-02 15:59:46 -0300

    se agrega cambios para mejorar el servidor

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authentication');
+const authController = require('../controllers/authentications');
 
 
 router.get('/inicio', authController.login);
