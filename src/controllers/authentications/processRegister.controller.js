591bfe09e2ebf17d9af3cb447247a02569ca59c5
andrewflower-rs <arnaldomahone@hotmail.com>
2024-03-28 21:09:51 -0300

    agrego funcionalidades de login y registro

---
+++ src/controllers/authentications/processRegister.controller.js
@@ -0,0 +1,29 @@
+const bcrypt = require ('bcryptjs');
+const { readData, saveData } = require('../../data');
+
+module.exports = (req,res)=>  {
+    const { name,email,password } = req.body; // Extrae los valores name, surname, email, y password
+    const users = readData("users"); // Obtiene los datos de los usuarios existentes
+  const newUser = { //: Crea un objeto para representar al nuevo usuario
+    id: !users.length ? 1 : users[users.length - 1].id + 1, /*Si no hay usuarios (!users.length), se asigna el id 1.
+    Si hay usuarios, se asigna el id del último usuario*/
+    name: name?.trim(),
+    surname: surname?.trim(),
+    email: email?.trim(),
+    password: bcrypt.hashSync(password?.trim(), 10), // La contraseña del usuario hasheada usando bcrypt.hashSync(password?.trim(), 10).
+    role: "REGULAR",
+    direccion: {
+     
+    },
+  };
+
+  users.push(newUser);  //: Agrega el objeto newUser que representa al nuevo usuario al final del arreglo users.
+
+  saveData(users, "users"); //guarda los datos actualizados del arreglo 
+
+  res.redirect("/"); // Redirige al cliente a la página raíz despus de registrarse correctamente
+};
+
+
+/*NOTAS :.trim() elimina espacios iniciales y finales.
+bcrypt.hashSync genera un hash seguro de la contraseña con un costo de 10 (puede ajustarse).*/
