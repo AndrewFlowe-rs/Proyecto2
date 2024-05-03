e94a533dc7bfd39398fa4b43346fbf45b9b85bcb
ElTataEmi <turasis88@gmail.com>
2024-05-03 00:51:36 -0300

    Migracion a la tabla Products

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Categoty',
+    modelName: 'Category',
   });
   return Categoty;
 };
