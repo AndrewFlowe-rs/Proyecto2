2fbd768e669af6a22587261cd7735a61c29d5531
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-05-26 01:54:59 -0300

    se valida el campo del nombre en el registro

--- public/js/validations/validationRegister.js
+++ public/js/validations/validationRegister.js
@@ -9,17 +9,33 @@ let password = document.getElementById('password')
 let number = document.getElementById('num')
 let ciudad =document.getElementById('direction')
 
-nombre.addEventListener('load' ,() => {
+window.addEventListener('load' ,() => {
 
 
     nombre.addEventListener('blur' , function ()  {
+const value = this.value.trim();
+const errName = document.querySelector('.msg-err')
+const expressAlfanumeric = /^([a-zA-Z0-9_-])$/
+const longitudMinima = 3;
+const longitudMaxima = 16;
 
+switch (true) {
+    case value.length === 0:
+        errName.innerHTML = 'el nombre es requerido'
+        this.classList.add ('is-invalid')
+
+        case !expressAlfanumeric.test(value):
+            errName.innerHTML = 'el nombre debe ser alfanumerico'
+            this.classList.add ('is-invalid')
+
+            case value.length  < longitudMinima || value.length > longitudMaxima :
+
+            errName.innerHTML = 'el nombre debe  tener entre 3 y 16 caracteres'
+        break;
+
+    default:
+        break;
+}
     })
 })
 
-form.addEventListener("submit", (event) => {
-    if (nombreInput.value.trim() === "") {
-        alert("Por favor, escribe tu nombre.");
-        event.preventDefault();
-    }
-})
