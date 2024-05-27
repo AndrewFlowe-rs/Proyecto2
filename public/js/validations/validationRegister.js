8c1b5ec85d2f4e89f94f781fe1233569f8fd7bd1
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-05-27 00:05:09 -0300

    se corrigen errores

--- public/js/validations/validationRegister.js
+++ public/js/validations/validationRegister.js
@@ -11,7 +11,7 @@ window.addEventListener("load", () => {
     let errName = document.querySelector(".msg-err");
     const expressAlfanumeric = /^([a-zA-Z0-9_-])$/;
     let existerr = true;
-    switch (existerr ) {
+    switch (existerr) {
       case value.length === 0:
         errName.innerHTML = "el nombre es requerido";
         this.classList.add("is-invalid");
@@ -30,44 +30,51 @@ window.addEventListener("load", () => {
         existerr = false;
         break;
     }
-    nombre.addEventListener('focus', function () {
-        errName.innerHTML = null;
-        this.classList.remove("is-invalid");
-        this.classList.remove("is-valid");
-    })
+    nombre.addEventListener("focus", function () {
+      errName.innerHTML = null;
+      this.classList.remove("is-invalid");
+      this.classList.remove("is-valid");
+    });
   });
 
-  campoEmail.addEventListener('blur', function() {
+  campoEmail.addEventListener("blur", function () {
     let errNamer = document.querySelector(".msge-err");
-     const email = campoEmail.value.trim();
-     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-     if (!emailRegex.test(email)) {
-         errNamer.innerHTML = 'El mail no es válido';
-     } else {
-        errNamer.innerHTML = '';
-     } 
-     campoEmail.addEventListener('focus', function(){
-        errNamer.innerHTML = ''
-        
-     })
- });
+    const email = campoEmail.value.trim();
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailRegex.test(email)) {
+      errNamer.innerHTML = "El mail no es válido";
+    } else {
+      errNamer.innerHTML = "";
+    }
+    campoEmail.addEventListener("focus", function () {
+      errNamer.innerHTML = "";
+    });
+  });
 
-    password.addEventListener('blur', function() {
-        let value = this.value.trim
-        let existerr=true
-        let errpasword = document.querySelector('.err-p')
-        switch (existerr) {
-            case value.length === 0:
-                errpasword.innerHTML = 'La contraseña es requerida'
+  password.addEventListener("blur", function () {
+    let value = this.value.trim;
+    let existerr = true;
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-                
-                break;
+    let errpasword = document.querySelector(".err-p");
+    switch (existerr) {
+      case value.length === 0:
+        errpasword.innerHTML = "La contraseña es requerida";
+
+        break;
+
+        case regex.test(value)  :
+            errpasword.innerHTML = 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número'
+
+      default:
+        errpasword.innerHTML = null;
+        existerr = false;
+        break;
         
-            default:
-                break;
-        }
-    })
-   
+    }
+    password.addEventListener
+  });
+
   //formulario
   form.addEventListener("submit", function (event) {
     event.preventDefault;
