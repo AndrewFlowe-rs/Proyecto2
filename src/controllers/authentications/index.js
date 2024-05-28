aff35dde68bd6b5665513e657ecabed100b0de9d
Al <aldananunez663@gmail.com>
2024-05-27 21:35:47 -0300

    Configurando botón logout

--- src/controllers/authentications/index.js
+++ src/controllers/authentications/index.js
@@ -2,5 +2,6 @@ module.exports = {
     login: require('./login.controller'),
     register: require('./register.controller'),
     loginProcess: require('./loginProcess.controller'),
-    processRegister : require ('./processRegister.controller')
+    processRegister : require ('./processRegister.controller'),
+    logout: require('./logout.controller')
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,10 +4,13 @@ const loginValidation = require('../middlewares/validation/loginValidation');
 const validProfile = require('../middlewares/validation/validProfile')
 const { profile, sesion } = require('../controllers/users')
 const { validationResult } = require('express-validator');
-const {loginProcess} = require('../controllers/authentications')
+const {loginProcess, logout} = require('../controllers/authentications')
 
 
 router.get('/',validProfile , sesion)
+router.get('/' , logout)
+
+
 router.post('/', loginValidation, loginProcess);
 
 
