5b764fdac40a4383ca7dd59075b9815429f4915a
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-06-13 01:16:42 -0300

    se agregan carpetas para que comienzen a trabajar

---
+++ dashboard/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const productos = props => {
+  return (
+    <div>productos</div>
+  )
+}
+
+productos.propTypes = {}
+
+export default productos
