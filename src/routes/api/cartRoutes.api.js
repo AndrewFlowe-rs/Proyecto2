5f0758b7a18f82ae45c0397ec7c372eb05b43911
Al <aldananunez663@gmail.com>
2024-06-27 20:38:31 -0300

    Limit a la página de productos

---
+++ src/routes/api/cartRoutes.api.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const {} = require('../../controllers/api/cart')
+
+// router('/', getCart)
+// router.patch('/add-product', addCart)
+// router.patch('/remove-product', removeCart)
+// router.patch('/more-quantity', moreQuantity)
+// router.patch('/less-quantity', lessQuantity)
+// router.delete('/remove-cart', deleteCart)
+
+
+
+
+
+
+module.exports = router
