4d08d420adb3e46926f9cdd0a0e9202815460e0a
Aldana Nuñez <aldananunez663@gmail.com>
2024-07-16 18:00:17 -0300

    Carrito arriba

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -1,34 +1,47 @@
-const decrementBtn = document.querySelector('.decrement-quantity');
-const incrementBtn = document.querySelector('.increment-quantity');
-const quantityDisplay = document.querySelector('.quantity');
-const totalDisplay = document.querySelector('.total span')
-const price = parseFloat('<%= p.price %>');
-
-
-let quantity = 1; // Cantidad inicial
-
-// Función para decrementar la cantidad
-decrementBtn.addEventListener('click', () => {
-    if (quantity > 1) {
-        quantity--;
-        updateQuantityAndTotal();
+const createAlert = ({type, title, timer}) => {
+    Swal.fire({
+      position: "top-end",
+      title,
+      showConfirmButton: false,
+      timer
+    });
+  }
+  
+  
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+  
+  
+  
+  const addProductCart = async (id) => {
+    const server = "http://localhost:3031";
+    try {
+      const { ok, msg } = await fetch(`${server}/api/cart/add/${id}?idUser=2`, {
+        method: "PATCH",
+      }).then((res) => res.json());
+  
+      ok && 
+      toastr["success"]("Producto agregado al carrito con éxito")
+      // createAlert({type:"success", title:"", timer: 500})
+    } catch (error) {
+      console.error(error.message);
     }
-});
-
-// Función para incrementar la cantidad
-incrementBtn.addEventListener('click', () => {
-    quantity++;
-    updateQuantityAndTotal();
-});
-
-// Función para actualizar la cantidad y el total
-function updateQuantityAndTotal() {
-    quantityDisplay.textContent = quantity;
-    totalDisplay.textContent = toThousand(quantity * price);
-}
-
-// Función para formatear el precio con separadores de miles
-function toThousand(price) {
-    return price.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' });
-}
-updateQuantityAndTotal();
+  };
+  
+  
+  
